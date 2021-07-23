c003c22a0a6fa6e4f8d963385aa58bd1ec8e6106
Camzero <u5500360@unimilitar.edu.co>
2021-07-23 11:09:00 +0800

    Title 2

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ function App() {
     <div>
       <h1>My Todo </h1>
       <div className='card'>
-        <h2>TITLE</h2>
+        <h2>TITLE </h2>
         <div className='actions'>
           <button className='btn'>Delete </button>
         </div>
