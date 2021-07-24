55dd07e163aafd1dae80d6cfd64a2d90dfa14dfe
Camzero <u5500360@unimilitar.edu.co>
2021-07-24 11:15:27 +0800

    Responsive web

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
+import Todo from './components/Todo';
+
 function App() {
   return (
     <div>
-      <h1>My Todo </h1>
-      <div className='card'>
-        <h2>TITLE </h2>
-        <div className='actions'>
-          <button className='btn'>Delete </button>
-        </div>
+      <div className='title'>
+        <h1>App My Todos</h1>
+      </div>
+      <div className='container'>
+        <Todo text='Learn React' />
+        <Todo text='Master React' />
+        <Todo text='Discover React' />
+        <Todo text='Explore React' />
       </div>
     </div>
   );
